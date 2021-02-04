79ef05e4cf57556bfd5f80578d7f98a5aec299a4
danny <danishmultani2001@gmail.com>
2021-02-04 14:17:15 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:exam1/Screens/home_screen.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:exam1/components/drawer_tile.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,6 +21,30 @@ class _MyAppState extends State<MyApp> {
   int selectedPage = 0;
   PageController _pageController = PageController(initialPage: 0);
   bool themeContent = false;
+
+  final BorderRadius _borderRadius = const BorderRadius.only(
+    topLeft: Radius.circular(25),
+    topRight: Radius.circular(25),
+  );
+
+  ShapeBorder bottomBarShape = const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(25)),
+  );
+  SnakeBarBehaviour snakeBarStyle = SnakeBarBehaviour.floating;
+  EdgeInsets padding = const EdgeInsets.all(12);
+
+  SnakeShape snakeShape = SnakeShape.circle;
+
+  bool showSelectedLabels = true;
+  bool showUnselectedLabels = true;
+
+  Color selectedColor = Colors.black;
+  Gradient selectedGradient =
+      const LinearGradient(colors: [Colors.red, Colors.amber]);
+
+  Color unselectedColor = Colors.white;
+  Gradient unselectedGradient =
+      const LinearGradient(colors: [Colors.red, Colors.blueGrey]);
 
   @override
   Widget build(BuildContext context) {
@@ -144,17 +169,37 @@ class _MyAppState extends State<MyApp> {
             ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: themeContent ? Colors.black54 : Colors.white,
-          type: BottomNavigationBarType.fixed,
-          selectedLabelStyle: TextStyle(fontSize: 20),
-          unselectedLabelStyle: TextStyle(fontSize: 18),
-          unselectedIconTheme: IconThemeData(size: 22),
-          selectedIconTheme: IconThemeData(size: 24),
-          unselectedItemColor: themeContent ? Colors.white : Colors.black,
+        bottomNavigationBar: SnakeNavigationBar.color(
+          behaviour: snakeBarStyle,
+          snakeShape: snakeShape,
+          shape: bottomBarShape,
+          padding: padding,
+          selectedLabelStyle: TextStyle(color: Colors.white),
+          unselectedLabelStyle:
+              TextStyle(color: themeContent ? Colors.white : Colors.black),
+
+          ///configuration for SnakeNavigationBar.color
+          snakeViewColor: selectedColor,
           selectedItemColor:
-              themeContent ? Colors.redAccent : Colors.blueAccent,
+              snakeShape == SnakeShape.indicator ? selectedColor : null,
+          unselectedItemColor: themeContent ? Colors.white : Colors.black,
+
+          ///configuration for SnakeNavigationBar.gradient
+          //snakeViewGradient: selectedGradient,
+          //selectedItemGradient: snakeShape == SnakeShape.indicator ? selectedGradient : null,
+          //unselectedItemGradient: unselectedGradient,
+
+          showUnselectedLabels: showUnselectedLabels,
+          showSelectedLabels: showSelectedLabels,
+
           currentIndex: selectedPage,
+          onTap: (newindex) {
+            setState(() {
+              selectedPage = newindex;
+              _pageController.animateToPage(newindex,
+                  duration: Duration(milliseconds: 500), curve: Curves.ease);
+            });
+          },
           items: [
             BottomNavigationBarItem(
               icon: Icon(Icons.home_outlined),
@@ -179,14 +224,50 @@ class _MyAppState extends State<MyApp> {
               label: 'World',
             ),
           ],
-          onTap: (newindex) {
-            setState(() {
-              selectedPage = newindex;
-              _pageController.animateToPage(newindex,
-                  duration: Duration(milliseconds: 500), curve: Curves.ease);
-            });
-          },
         ),
+        // BottomNavigationBar(
+        //   backgroundColor: themeContent ? Colors.black54 : Colors.white,
+        //   type: BottomNavigationBarType.fixed,
+        //   selectedLabelStyle: TextStyle(fontSize: 16),
+        //   unselectedLabelStyle: TextStyle(fontSize: 14),
+        //   unselectedIconTheme: IconThemeData(size: 20),
+        //   selectedIconTheme: IconThemeData(size: 24),
+        //   unselectedItemColor: themeContent ? Colors.white : Colors.black,
+        //   selectedItemColor:
+        //       themeContent ? Colors.redAccent : Colors.blueAccent,
+        //   currentIndex: selectedPage,
+        //   items: [
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.home_outlined),
+        //       label: 'Home',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.trending_up),
+        //       label: 'Trending',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(
+        //         Icons.search,
+        //       ),
+        //       label: 'Search',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(MdiIcons.flaskOutline),
+        //       label: 'Science',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.language),
+        //       label: 'World',
+        //     ),
+        //   ],
+        //   onTap: (newindex) {
+        //     setState(() {
+        //       selectedPage = newindex;
+        //       _pageController.animateToPage(newindex,
+        //           duration: Duration(milliseconds: 500), curve: Curves.ease);
+        //     });
+        //   },
+        // ),
         body: PageView(
           controller: _pageController,
           onPageChanged: (index) {
